e91ae6668db290986a438e953497e19c7eb0dd13
Ahmed Islam <modamoon48@gmail.com>
2022-10-26 04:16:30 +1100

    Misc fixes

--- PostProcessing.py
+++ PostProcessing.py
@@ -2,37 +2,38 @@ import json
 
 
 def main() -> None:
-    file_names = ['anime', 'asian-series', 'movies', 'series', 'tvshows']
+    FILE_NAMES = ['movies', 'anime', 'asian-series', 'series', 'tvshows']
 
     with open('./output/image-indices.json', 'r') as fp:
         image_indices = json.load(fp)
 
-    for file in file_names:
+    for index, file in enumerate(FILE_NAMES):
         with open(f'./output/{file}.json', 'r') as fp:
             content = json.load(fp)
 
         for key in content:
             image_indices[key] = content[key]["Image Source"]
 
-        if file in ['asian-series', 'series', 'tvshows']:
+        if index in range(2, 5):
             for key in list(content.keys()):
                 if len(content[key]["Seasons"]) == 0:
                     del content[key]
                 else:
                     continue
 
-        elif file == 'movies':
+        elif index == 0:
             for key in list(content.keys()):
                 if content[key]["Source"] == "":
                     del content[key]
                 else:
                     continue
 
-            with open(f'./output/{file}.json', 'w') as fp:
-                json.dump(content, fp, indent=4)
         else:
             pass
 
+        with open(f'./output/{file}.json', 'w') as fp:
+            json.dump(content, fp, indent=4)
+        
     with open('./output/image-indices.json', 'w') as fp:
         json.dump(image_indices, fp, indent=4)
 
