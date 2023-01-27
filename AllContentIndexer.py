2b0da0fd49e3103c58ed787f7a27b050d4455f75
Ahmed Islam <modamoon48@gmail.com>
2023-01-27 20:56:57 +1100

    Fixed all content

--- AllContentIndexer.py
+++ AllContentIndexer.py
@@ -15,12 +15,13 @@ def main() -> None:
             else:
                 rating = "N/A"
 
-            all_content += {"key": key,
+            all_content += [{"key": key,
                             "Title": data[key]["Title"],
-                            "Image Source": data[key]["Image Source"],
-                            "Category": data[key]["Category"],
-                            "Genres": data[key]["Genres"],
-                            "Rating": rating}
+                             "Image Source": data[key]["Image Source"],
+                             "Category": data[key]["Category"],
+                             "Genres": data[key]["Genres"],
+                             "Rating": rating
+                             }]
 
     with open("./output/all-content.json", "w", encoding="utf-8") as fp:
         json.dump({"content": all_content}, fp, indent=4, ensure_ascii=False)
